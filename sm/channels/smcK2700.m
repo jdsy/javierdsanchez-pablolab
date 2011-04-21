cdb2a745e1f57a36a8cb45b3c4a4506b806f8d74
jdsy <jdsy@localhost>
2011-04-21 15:25:35 -0400

    Fixed formatting of K2700

--- sm/channels/smcK2700.m
+++ sm/channels/smcK2700.m
@@ -7,7 +7,9 @@ global smdata;
 switch ic(3); %Operation: 0 for read, 1 for write
 
     case 0 %just read voltage for now
-        val = query(smdata.inst(ic(1)).data.inst, 'DATA?', '%s\n', '%f');
+        val_char=query(smdata.inst(ic(1)).data.inst,':data?');
+        val_str=char(val_char(1:15));
+        val=str2num(val_str);
                   
     case 1 %write operation;  
         error('The DMM does not support write operations');
