18f76ac10b0b1d28a8bb1d299e39c8c051aa922b
Hadar Steinberg <Hadar Steinberg@BeloHorizonte.mit.edu>
2011-05-10 09:00:58 -0400

    init.m

--- Measurement Setups/He3 Setup/init.m
+++ Measurement Setups/He3 Setup/init.m
@@ -1,26 +1,28 @@
 
-
-instrreset;
-%recover
-if 1==1
-  clear all;
-
-% load('Z:\group\measurements\leonardo\Bi_layer_device\BN_01_11_11_BN14\He3_measurements\pn1\ScansDC\Buffer_Rack.mat');  %rack v5 has corrections to DAC
-load('Z:\group\measurements\leonardo\Bi_layer_device\BN_01_11_11_BN14\He3_measurements\pn1\ScansDC\Buffer_Rack_v2.mat');  %rack v5 has corrections to DAC
-
-end
 global smdata; 
 global smscan;
 
+%recover
+if 1==1
+    clear all;
+    global smdata; 
+    global smscan;
+    % load('Z:\group\measurements\leonardo\Bi_layer_device\BN_01_11_11_BN14\He3_measurements\pn1\ScansDC\Buffer_Rack.mat');  %rack v5 has corrections to DAC
+    load('Y:\group\measurements\leonardo\Bi_layer_device\BN_02_23A\MatLab_scans\Rack_Leonardo_2011_04_27.mat');  %rack v5 has corrections to DAC
+
+end
+instrreset;
+
+
 %Open communications for all instruments
-k2400adr = 14;
+k2400adr = 14;          % Addressing
 k2700adr = 16;
 sr830adr = 8;
 sr830badr = 9;
 Tcontadr = 12;
 MagContadr = 1;
 
-sr830ind = 2;
+sr830ind = 2;            % Indexing
 k2400ind = 3;
 k2700ind = 4;
 dacind = 5;
@@ -31,7 +33,7 @@ MagContind = 8;
 smdata.inst(sr830ind).data.inst = gpib('ni', 0, sr830adr);
 smdata.inst(k2400ind).data.inst = gpib('ni', 0, k2400adr);
 smdata.inst(k2700ind).data.inst = gpib('ni', 0, k2700adr);
-smdata.inst(dacind).data.inst = serial('COM4');
+smdata.inst(dacind).data.inst = serial('COM1');
 smdata.inst(sr830bind).data.inst = gpib('ni', 0, sr830badr);
 smdata.inst(Tcontind).data.inst = gpib('ni', 0, Tcontadr);
 smdata.inst(MagContind).data.inst = gpib('ni', 0, MagContadr);
@@ -76,8 +78,8 @@ end
 if 1==0
     fprintf(smdata.inst(MagContind).data.inst,'UNITS T');
     fprintf(smdata.inst(MagContind).data.inst,'RANGE 0 39.46');
-    fprintf(smdata.inst(MagContind).data.inst,'RATE 0 0.02');
+    fprintf(smdata.inst(MagContind).data.inst,'RATE 0 0.02');   % rate 4.6mT/s
     fprintf(smdata.inst(MagContind).data.inst,'PSHTR ON');
 end
-cd('Z:\group\measurements\leonardo\Bi_layer_device\BN_01_11_11_BN14\He3_measurements\pn1');
+cd('Y:\group\measurements\leonardo\Bi_layer_device\BN_02_23A\Bi_Layer_2_3');
 smgui;
