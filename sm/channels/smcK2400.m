469226b389da7e6f594edf169dc5dd3b9ff979c6
Hadar Steinberg <Hadar Steinberg@BeloHorizonte.mit.edu>
2011-05-10 09:00:42 -0400

    typo too many end statements at the end

--- sm/channels/smcK2400.m
+++ sm/channels/smcK2400.m
@@ -1,53 +1,47 @@
 function val = smcK2400(ic, val, rate)
 
-%Driver for Keithley 2400
-%Last update: Hadar 10-13-2010
+    %Driver for Keithley 2400
+    %Last update: Hadar 10-13-2010
 
-global smdata;
+    global smdata;
 
-strchan = smdata.inst(ic(1)).channels(ic(2),:);
-if strchan == 'VSTEP'
+    strchan = smdata.inst(ic(1)).channels(ic(2),:);
+    if strchan == 'VSTEP'
 
-    switch ic(3); %Operation: 0 for read, 1 for write
-
-        case 0 %read
-            %'case 0'
-            KO = query(smdata.inst(ic(1)).data.inst, 'READ?', '%s\n', '%f,%f,%f,%f,%f');
-            val = KO(1);
-
-        case 1 %write operation; 
-            %'case 1'
-            cmd = ':SOUR:VOLT ';
-            fprintf(smdata.inst(ic(1)).data.inst, sprintf('%s %f', cmd, val));
-
-        otherwise
-            error('Operation not supported');
-    end
-else
-    if strchan == 'ISTEP'
         switch ic(3); %Operation: 0 for read, 1 for write
 
-        case 0 %just read voltage for now
-            %'case 0'
-            KO = query(smdata.inst(ic(1)).data.inst, 'READ?', '%s\n', '%f,%f,%f,%f,%f');
-            val = KO(2);
+            case 0 %read
+                %'case 0'
+                KO = query(smdata.inst(ic(1)).data.inst, 'READ?', '%s\n', '%f,%f,%f,%f,%f');
+                val = KO(1);
 
-        case 1 %write operation;  
-            %'case 1'
-            cmd = ':SOUR:CURR ';
-            fprintf(smdata.inst(ic(1)).data.inst, sprintf('%s %f', cmd, val));
+            case 1 %write operation; 
+                %'case 1'
+                cmd = ':SOUR:VOLT ';
+                fprintf(smdata.inst(ic(1)).data.inst, sprintf('%s %f', cmd, val));
 
-        otherwise
-            error('Operation not supported');
+            otherwise
+                error('Operation not supported');
+        end
+    else
+        if strchan == 'ISTEP'
+            switch ic(3); %Operation: 0 for read, 1 for write
+
+            case 0 %just read voltage for now
+                %'case 0'
+                KO = query(smdata.inst(ic(1)).data.inst, 'READ?', '%s\n', '%f,%f,%f,%f,%f');
+                val = KO(2);
+
+            case 1 %write operation;  
+                %'case 1'
+                cmd = ':SOUR:CURR ';
+                fprintf(smdata.inst(ic(1)).data.inst, sprintf('%s %f', cmd, val));
+
+            otherwise
+                error('Operation not supported');
+            end
         end
     end
-end
-        
-end
-
-        end
-    end
-end
         
 end
 
