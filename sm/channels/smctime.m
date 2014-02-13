520a8626d4f36f7a0aa795a404e9b34af4edf23f
jdsy <jdsy@localhost>
2014-02-13 10:32:57 -0500

    Updates to time instrument so that it just returns the current time

--- sm/channels/smctime.m
+++ sm/channels/smctime.m
@@ -6,8 +6,14 @@ global smdata;
 
 switch ic(3); %Operation: 0 for read, 1 for write
 
-    case 0 %just read time
-        val=toc;
+    case 0 %read
+        switch ic(2)
+            case 1 %toc
+                val=toc;
+            case 2
+                val=now;
+        end
+        
                   
     case 1 %write operation;  
         error('Unfortunately, this program cannot control Time... yet');
